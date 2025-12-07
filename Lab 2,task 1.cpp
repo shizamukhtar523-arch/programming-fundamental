548a705fd13c3868e20f03195fc328556dce04fc
shizamukhtar523-arch <shizamukhtar523@gmail.com>
2025-12-07 03:15:15 -0800

    Delete Lab 2,task 1.cpp

--- Lab 2,task 1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main ()
-{
-    cout << "Welcome to the C++ Lab!" << endl;
-    cout << "You will learn basic output today." << endl;
-    cout << "Let's start with simple messages." << endl;
-    return 0;
-}
